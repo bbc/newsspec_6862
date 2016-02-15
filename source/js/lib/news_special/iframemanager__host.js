4f9cd12571c4943ff5bdf9860f4f312e67e1fd58
Aidan Fewster <fewstera@gmail.com>
2016-02-15 12:24:20 +0000

    Update iframemanager__host.js

--- source/js/lib/news_special/iframemanager__host.js
+++ source/js/lib/news_special/iframemanager__host.js
@@ -75,9 +75,6 @@
                 this.data.setup &&
                 (!this.iframeInstructionsRan)
             ) {
-                /* jshint evil: true */
-                eval("var func = " + this.data.setup);
-                func();
                 this.iframeInstructionsRan = true;
             }
         },
